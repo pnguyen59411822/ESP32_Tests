43bd118d1c42d0956e7a78ac621f1c0b0e9205a0
nguyendaiphuc151006 <nguyendaiphuc151006@gmail.com>
2023-03-10 14:21:20 +0700

    - Cài đặt thư viện DHT - Cài đặt thư viện eFLL - Viết thư viện DHT_service từ code mẫu

---
+++ ESP32_Tests/lib/DHT/DHT_service.cpp
@@ -0,0 +1,112 @@
+/* ==================================================
+** Libraries
+**
+** =============================================== */
+
+
+#include "DHT_service.h"
+#include "DHT_const.h"
+
+
+/* ==================================================
+** Macro definition
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Type definition
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Extern variables
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Global variables
+**
+** =============================================== */
+
+
+DHT dht(DHT_PIN, DHT_TYPE);
+
+// Huminity
+float huminity;
+// Temperature C
+float tempC;
+// Temperature F
+float tempF;
+// Heat index C
+float hiC;
+// Heat index F
+float hiF;
+
+uint32_t intv_wait_upd;
+
+
+/* ==================================================
+** Global function declaration
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Global function definition
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Extern function definition
+**
+** =============================================== */
+
+
+void DHT_init()
+{
+    dht.begin();
+}
+
+
+void DHT_upd()
+{
+    if(millis() - intv_wait_upd < TIME_WAIT_UPD) {return;}
+
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    float f = dht.readTemperature(true);
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t) || isnan(f))
+    {
+        Serial.println(F("Failed to read from DHT sensor!"));
+        return;
+    }
+
+    huminity    = h;
+    tempC       = t;
+    tempF       = f;
+    hiC         = dht.computeHeatIndex(t, h, false);
+    hiF         = dht.computeHeatIndex(t, h);
+
+    Serial.printf("(%u) [DHT][UPD] Humidity: %.2f% | Temperature: %.2f°C  %.2f°F | Head index: %.2f°C  %.2f°F\n", millis(), h, t, f, hiC, hiF);
+}
