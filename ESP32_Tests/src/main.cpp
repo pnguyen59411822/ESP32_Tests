8a859365d9b4ebf8bf91f3ccccc0b87e806931a4
nguyendaiphuc151006 <nguyendaiphuc151006@gmail.com>
2023-03-14 12:20:23 +0700

    scan địa chỉ I2C

--- ESP32_Tests/src/main.cpp
+++ ESP32_Tests/src/main.cpp
@@ -1,9 +1,51 @@
 #include <Arduino.h>
+#include <Wire.h>
+
+
+void Scanner();
+
 
 void setup() {
+  Serial.begin(115200);
+  Wire.begin();
   // put your setup code here, to run once:
 }
 
 void loop() {
+  Scanner();
+  delay(5000);
   // put your main code here, to run repeatedly:
+}
+
+void Scanner ()
+{
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  }
+  else {
+    Serial.println("done\n");
+  }
 }
