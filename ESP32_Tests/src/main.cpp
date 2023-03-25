b70d68db7e7688e4c59a7219d28ae864f7f8b9e8
nguyendaiphuc151006 <nguyendaiphuc151006@gmail.com>
2023-03-25 10:40:49 +0700

    Đọc cảm biến từ chân 34 Độ phân giải là 4095 In giá trị analog và phần trăm

--- ESP32_Tests/src/main.cpp
+++ ESP32_Tests/src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
 
+#define SM_PIN     34
+
+#define SM_IN_MAX     4095
+#define SM_IN_MIN     0
+
+#define SM_OUT_MAX    100
+#define SM_OUT_MIN    0
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(1000);
+  Serial.print(F("\n\n"));
+
+  pinMode(SM_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float raw = analogRead(SM_PIN);
+  float per = (raw-SM_IN_MIN) / (SM_IN_MAX-SM_IN_MIN) * (SM_OUT_MAX-SM_OUT_MIN) + SM_OUT_MIN;
+  Serial.printf("raw: %7.2f | percent: %7.2f\n", raw, per);
+  delay(1000);
 }
