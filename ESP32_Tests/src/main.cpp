17d197d5cf95e2d9e14e7094a7947a8f7a521dd7
nguyendaiphuc151006 <nguyendaiphuc151006@gmail.com>
2023-03-25 10:52:40 +0700

    Copy code example

--- ESP32_Tests/src/main.cpp
+++ ESP32_Tests/src/main.cpp
@@ -1,9 +1,96 @@
 #include <Arduino.h>
 
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+
+#define seaLevelPressure_hPa 1013.25
+
+
+void Scanner();
+
+Adafruit_BMP085 bmp;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(1000);
+  Serial.print(F("\n\n"));
+
+
+  if (!bmp.begin()) 
+  {
+    Serial.println("Could not find a valid BMP085/BMP180 sensor, check wiring!");
+
+    while (1) {
+      Scanner();
+      delay(2000);
+    }
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  
+void loop() 
+{
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+    
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+    
+  // Calculate altitude assuming 'standard' barometric
+  // pressure of 1013.25 millibar = 101325 Pascal
+  Serial.print("Altitude = ");
+  Serial.print(bmp.readAltitude());
+  Serial.println(" meters");
+
+  Serial.print("Pressure at sealevel (calculated) = ");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.println(" Pa");
+
+  // you can get a more precise measurement of altitude
+  // if you know the current sea level pressure which will
+  // vary with weather and such. If it is 1015 millibars
+  // that is equal to 101500 Pascals.
+  Serial.print("Real altitude = ");
+  Serial.print(bmp.readAltitude(seaLevelPressure_hPa*100));
+  Serial.println(" meters");
+    
+  Serial.println();
+  delay(500);
+}
+
+
+void Scanner ()
+{
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  }
+  else {
+    Serial.println("done\n");
+  }
 }
